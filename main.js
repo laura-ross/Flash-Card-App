4b6c6f91eb04ccd83cf5b7867f2664683d6a61c4
LauraE2020 <smalltowngirl591@gmail.com>
2020-08-30 13:11:29 -0400

    Completed functionality

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// 1. Note class
+// 2. UI class - add & remove note, clearInputs, validation, alerts
+// 3. Storage class - local storage
+// 4. Events
+  // 4.1 Display notes
+  // 4.2 Add note
+  // 4.3 Remove note
+
+
 // 1. Note Class
 class Note {
   constructor(title, body, id) {
@@ -7,16 +16,14 @@ class Note {
   }
 }
 
-// 2. UI Class
+// 2. UI Class - add & remove note, clearInputs, validation, alerts
 class UI {
   static displayNotes() {
     const notes = Store.getNotesFromStore();
-
     notes.forEach((note) => UI.addNote(note))
   }
 
   static addNote(note) {
-
     const notesWrapper = document.querySelector(".notes-wrapper");
     const card =  document.createElement("div");
     card.classList.add("card", "m-2");
@@ -25,8 +32,7 @@ class UI {
         <h4 class="card-title">${note.title}</h4>
         <p class="card-text">${note.body}</p>
         <span data-id=${note.id}></span>
-        <button class="btn btn-sm btn-success edit">Edit</button>
-        <button class="btn btn-sm btn-danger remove">Remove</button>
+        <button class="btn btn-sm btn-danger remove mt-3">Remove</button>
       </div>
     `;
 
@@ -37,43 +43,6 @@ class UI {
     if(element.classList.contains("remove")) {
       element.parentElement.parentElement.remove();
     }
-  }
-
-  static editNote(element) {
-    let cardTitle = document.querySelector(".card-title").innerText;
-    let cardBody = document.querySelector(".card-text").innerText;
-    let title = document.querySelector(".title");
-    let body = document.querySelector(".body");
-
-
-    if(element.classList.contains("edit")) {
-      const form = document.querySelector(".form");
-      const addBtn = document.querySelector(".add-note-btn");
-      title.value = cardTitle;
-      body.value = cardBody;
-
-      // Create a save button
-      const saveBtn = document.createElement("button");
-      saveBtn.classList.add("btn", "btn-info", "btn-block", "m-2", "save-btn");
-      saveBtn.innerText = "Save";
-      form.appendChild(saveBtn);
-
-      // Create a cancel button
-      const cancelBtn = document.createElement("button");
-      cancelBtn.classList.add("btn", "btn-danger", "btn-block", "m-2", "cancel-btn")
-      cancelBtn.innerText = "Cancel";
-      form.appendChild(cancelBtn);
-
-      // Remove the add button
-      addBtn.remove();
-      }
-  }
-
-  static updateNoteValues(element) {
-    let cardTitle = document.querySelector(".card-title").innerText;
-    let cardBody = document.querySelector(".card-text").innerText;
-    let title = document.querySelector(".title");
-    let body = document.querySelector(".body");
   }
 
   // Alerts function
@@ -116,7 +85,7 @@ class Store {
 
   static removeNoteFromStore(id) {
     const notes = Store.getNotesFromStore();
-
+    // loop thru notes to check if id matches selected id
     notes.forEach((note, index) => {
       if(note.id == id) {
         notes.splice(index, 1);
@@ -124,13 +93,8 @@ class Store {
     });
     localStorage.setItem("notes", JSON.stringify(notes));
   }
-
-  static editNoteInStore(id) {
-    // click Edit button
-    // values appear in form
-    // note disappears from UI?
-  }
 }
+
 // 4. Events:
 
   // 4.1 Display notes
@@ -144,12 +108,6 @@ class Store {
     const notes = Store.getNotesFromStore();
     let id = Date.now();
 
-    //if(notes.length > 0) {
-    //  id = notes.indexOf()
-    //}
-
-
-
     // Validation
     if(title === " ") {
       UI.showAlert("Please fill in the title field", "danger");
@@ -162,71 +120,12 @@ class Store {
     }
 
   });
+
   // 4.3 Remove note
   document.querySelector(".notes-wrapper").addEventListener("click", e => {
     if(e.target.classList.contains("remove")) {
       UI.removeNote(e.target);
       UI.showAlert("Note removed", "success");
       Store.removeNoteFromStore(e.target.previousElementSibling.previousElementSibling.dataset.id);
-    }
-  });
-  // 4.4 Edit note (click edit btn)
-  document.querySelector(".notes-wrapper").addEventListener("click", e => {
-
-
-    if(e.target.classList.contains("edit")) {
-
-      UI.editNote(e.target);
-      //console.log(e.target.previousElementSibling.dataset.id)
-
-    }
-  });
-
-  // 4.5 Cancel edit form
-document.querySelector(".form").addEventListener("click", (e) => {
-  if(e.target.classList.contains("cancel-btn")) {
-    // remove save Btn
-    document.querySelector(".save-btn").remove();
-    // remove cancel Btn
-    document.querySelector(".cancel-btn").remove();
-    // Add the original add Btn
-    const form = document.querySelector(".form");
-    const addBtn = document.createElement("button");
-    addBtn.classList.add("btn", "btn-success", "btn-block", "m-2", "add-note-btn");
-    addBtn.innerText = "Add Note";
-    form.appendChild(addBtn);
-
-    // Remove values from form
-    UI.clearInputs();
-  }
-});
-
-  // 4.6 Save edit form / Update form values
-  document.querySelector(".form").addEventListener("click", e => {
-    //const title = document.querySelector(".title").value;
-    //const body = document.querySelector(".body").value;
-    e.preventDefault();
-    if(e.target.classList.contains("save-btn")) {
-      //UI.updateNoteValues(e.target);
-      let title = document.querySelector(".title");
-      let body = document.querySelector(".body");
-      let cardTitle = document.querySelector(".card-title");
-      let cardBody = document.querySelector(".card-text");
-
-      cardTitle.innerText = title.value;
-      cardBody.innerText = body.value;
-
-      UI.clearInputs();
-
-      // remove save Btn
-      document.querySelector(".save-btn").remove();
-      // remove cancel Btn
-      document.querySelector(".cancel-btn").remove();
-      // Add the original add Btn
-      const form = document.querySelector(".form");
-      const addBtn = document.createElement("button");
-      addBtn.classList.add("btn", "btn-success", "btn-block", "m-2", "add-note-btn");
-      addBtn.innerText = "Add Note";
-      form.appendChild(addBtn);
     }
   });
